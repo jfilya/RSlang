70532dc360715189457585a5169828b7d03d4e82
OreskaG <Ctepanrfo@gmail.com>
2022-08-20 18:00:37 +0300

    feat: add copy webpack plugin

--- RSLang/webpack.config.js
+++ RSLang/webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
   entry: path.resolve(__dirname, './src/index'),
@@ -49,6 +50,7 @@ const baseConfig = {
     }),
     new CleanWebpackPlugin(),
     new EslintPlugin({ extensions: 'ts' }),
+    new CopyPlugin({ patterns: [{ from: './src/assets', to: 'assets' }] }),
   ],
   experiments: {
     topLevelAwait: true,

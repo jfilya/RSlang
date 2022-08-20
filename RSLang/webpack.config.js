d462deab9eba7501204c5d80630c4b1b7ecae4cb
OreskaG <Ctepanrfo@gmail.com>
2022-08-20 15:31:56 +0300

    feat: add file-loader

--- RSLang/webpack.config.js
+++ RSLang/webpack.config.js
@@ -33,6 +33,14 @@ const baseConfig = {
                   "sass-loader",
                 ],
             },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
         ],
     },
     resolve: {
